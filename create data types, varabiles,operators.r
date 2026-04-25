9232e8f5a743369381918e39c533de121bead1ce
11249a349-boop <11249a349@kanchiuniv.ac.in>
2026-04-25 19:50:03 +0530

    Create create data types, varabiles,operators.r

---
+++ create data types, varabiles,operators.r
@@ -0,0 +1,115 @@
+roll_no <- 102 # Integer
+name <- "sireesha" # Character
+cgpa <- 8.40 # Numeric
+pass <- TRUE # Logical
+roll_no
+name
+cgpa
+pass
+
+emp_id <- 2001
+salary <- 65000.75
+department <- "IT"
+is_permanent <- TRUE
+emp_id
+salary
+department
+is_permanent
+ 
+product_name <- "fans"
+price <- 5000
+stock_available <- 25
+discount_eligible <- FALSE
+product_name
+price
+stock_available
+discount_eligible
+
+city <- "banglore"
+temperature <- 32.5
+rain <- FALSE
+wind_speed <- 12.8
+city
+temperature
+rain
+wind_speed
+ 
+subject <- "Mathematics"
+total_marks <- 100
+marks_obtained <- 78
+result <- marks_obtained >= 40
+subject
+total_marks
+marks_obtained
+result
+
+account_no <- 123456789
+holder_name <- "Ravi Kumar"
+balance <- 25000.50
+account_active <- TRUE
+account_no
+holder_name
+balance
+account_active
+ 
+account_no <- 123456789
+holder_name <- "Ravi Kumar"
+balance <- 25000.50
+account_active <- TRUE
+account_no
+holder_name
+balance
+account_active
+
+movie_title <- "Inception"
+rating <- 8.8
+reviews <- 1200
+available <- TRUE
+movie_title
+rating
+reviews
+available
+
+vehicle_count <- 350
+location <- "Highway Road"
+avg_speed <- 45.6
+congested <- TRUE
+vehicle_count
+location
+avg_speed
+congested
+
+vehicle_count <- 200
+location <- "Highway Road"
+avg_speed <- 65.6
+congested <- TRUE
+vehicle_count
+location
+avg_speed
+congested
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+4:
+Weather Monitoring Syste
+
